759669b68820170a159b1d13f17be6b357612c10
Jungi KIM <jungi.kim@systrangroup.com>
2024-02-02 21:15:54 -0800

    try fixing compilation on macos arm64

--- tools/prepare_build_environment.sh
+++ tools/prepare_build_environment.sh
@@ -4,13 +4,18 @@ set -e
 set -x
 
 ROOT_DIR=$PWD
+CMAKE_EXTRA_ARGS=""
 
-pip install "cmake==3.18.*"
+if [ "$CIBW_ARCHS" == "arm64" ]; then
+    CMAKE_EXTRA_ARGS="-DCMAKE_OSX_ARCHITECTURES=x86_64;arm64"
+fi
+
+pip install "cmake==3.28.1"
 
 rm -rf build
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_ROOT} -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_ROOT} -DCMAKE_BUILD_TYPE=Release $CMAKE_EXTRA_ARGS ..
 VERBOSE=1 make install
 cd $ROOT_DIR
 
